e90f40a9aa07fe9f356b7842e8f28465a1088248
samoyi <leening529@163.com>
2016-10-23 21:46:16 +0800

    function rename

--- MySQLiController.class.php
+++ MySQLiController.class.php
@@ -438,7 +438,7 @@ class MySQLiController
 	}
 
     // 修改列名
-    public function changeColumnName($tableName, $sColumn, $sNewname)
+    public function columnReame($tableName, $sColumn, $sNewname)
     {
         $aColumnInfo = $this->getColumnInfoArray($tableName, $sColumn);
         $sOldName = $aColumnInfo["Field"];
